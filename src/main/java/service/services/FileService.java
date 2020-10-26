c5433937e1d8c251820492310906b03e5ca9c6f0
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-10-26 23:43:15 +0700

    WIP: try to release multiroot base directories

--- src/main/java/service/services/FileService.java
+++ src/main/java/service/services/FileService.java
@@ -10,22 +10,28 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class FileService {
-  @Value("${service.base-dir}")
-  private String baseDirectoryPropertyValue;
-  private File baseDirectory;
+  @Value("#{'${service.base-dir}'.split(',')}")
+  private List<String> baseDirectoriesPropertyValue;
+  private final Map<String, File> baseDirectories = new HashMap<>();
 
   @PostConstruct
   private void init() {
-    this.baseDirectory = new File(baseDirectoryPropertyValue);
+    for (String path: baseDirectoriesPropertyValue) {
+      File file = new File(path);
+      if (!file.exists() || !file.canRead()) {
+        System.err.println("File " + path + " isn't exists or not readable!");
+        continue;
+      }
+      this.baseDirectories.put(StringUtils.isEmpty(file.getName()) ? file.getPath() : file.getName(), file);
+    }
   }
 
-  public service.models.File putFile(String path, MultipartFile file, boolean override, boolean createParentFolders) throws IOException {
-    File directory = checkAvailability(path, true, createParentFolders);
+  public service.models.File putFile(String parent, String path, MultipartFile file, boolean override, boolean createParentFolders) throws IOException {
+    File directory = checkAvailability(parent, path, true, createParentFolders);
     String originalFileName = file.getOriginalFilename();
     if (StringUtils.isEmpty(originalFileName) || StringUtils.isEmpty(originalFileName.trim()))
       throw new IllegalArgumentException("Request doesn't contains 'originalFilename'!");
@@ -37,39 +43,51 @@ public class FileService {
     return new service.models.File(newFile);
   }
 
-  public List<service.models.File> getFiles(String path, boolean showHiddenFiles) throws IOException {
-    File directory = checkAvailability(path, true);
+  public List<service.models.File> getFiles(String parent, String path, boolean showHiddenFiles) throws IOException {
+    List<service.models.File> files = new ArrayList<>();
+    if (!parent.equals("/")) {
+      File directory = checkAvailability(parent, path, true);
 
-    File[] directoryFiles = directory.listFiles();
-    List<service.models.File> files = new LinkedList<>();
-    if (directoryFiles != null) {
-      for (File file : directoryFiles) {
-        if (showHiddenFiles || !file.isHidden()) {
-          files.add(new service.models.File(file));
+      File[] directoryFiles = directory.listFiles();
+      if (directoryFiles != null) {
+        for (File file : directoryFiles) {
+          if (showHiddenFiles || !file.isHidden()) {
+            files.add(new service.models.File(file));
+          }
         }
+      }
+    } else {
+      for (Map.Entry<String, File> baseDirectory : baseDirectories.entrySet()) {
+        files.add(new service.models.File(baseDirectory.getValue()));
       }
     }
     return files;
   }
 
-  public service.models.File getFile(String path) throws IOException {
-    File file = checkAvailability(path, false);
+  public service.models.File getFile(String parent, String path) throws IOException {
+    File file = checkAvailability(parent, path, false);
     return new service.models.File(file);
   }
 
-  public boolean deleteFile(String path) throws IOException {
-    File file = checkAvailability(path, false);
+  public boolean deleteFile(String parent, String path) throws IOException {
+    File file = checkAvailability(parent, path, false);
     return file.delete();
   }
 
-  private File checkAvailability(String path, @Nullable Boolean itsDirectory) throws IOException {
-    return this.checkAvailability(path, itsDirectory, false);
+  private File checkAvailability(String parent, String path, @Nullable Boolean itsDirectory) throws IOException {
+    return this.checkAvailability(parent, path, itsDirectory, false);
   }
 
-  private File checkAvailability(String path, @Nullable Boolean itsDirectory, boolean createParentFolders) throws IOException {
-    File file = new File(baseDirectory, path);
+  private File checkAvailability(String parent, String path, @Nullable Boolean itsDirectory, boolean createParentFolders) throws IOException {
+    if (!baseDirectories.containsKey(parent)) {
+      throw new IOException("Base directory " + parent + " isn't available!");
+    }
+
+    File baseDirectory = baseDirectories.get(parent);
     if (!baseDirectory.exists() || !baseDirectory.canRead() || !baseDirectory.isDirectory())
-      throw new IOException("Base directory isn't available!");
+      throw new IOException("Base directory " + parent + " isn't available!");
+
+    File file = new File(baseDirectory, path);
 
     if (!file.getCanonicalPath().contains(baseDirectory.getCanonicalPath()))
       throw new IOException("File: " + file.getName() + " isn't allowed!");
