4920ca91f3a282280986c3ca4ab831103beec441
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-10-31 20:47:54 +0700

    Added RestControllerExceptionAdvice and resolve SecurityException

---
+++ src/main/java/service/controllers/advices/RestControllerExceptionAdvice.java
@@ -0,0 +1,29 @@
+package service.controllers.advices;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class RestControllerExceptionAdvice extends ResponseEntityExceptionHandler {
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @ExceptionHandler(value = SecurityException.class)
+  protected ResponseEntity<?> handleSecurityException(RuntimeException ex, WebRequest request) {
+    HttpStatus status = HttpStatus.FORBIDDEN;
+    ObjectNode errorObjectNode = objectMapper.createObjectNode();
+    errorObjectNode.put("status", status.value());
+    errorObjectNode.put("error", ex.getMessage());
+    return handleExceptionInternal(ex, errorObjectNode, new HttpHeaders(), status, request);
+  }
+}
