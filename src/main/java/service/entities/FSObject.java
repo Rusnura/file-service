7026e1b5f976f1e0a9e0181a5bf5a4096226fdb3
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-05-30 00:49:54 +0700

    Added parent property to FSObject

--- src/main/java/service/entities/FSObject.java
+++ src/main/java/service/entities/FSObject.java
@@ -2,9 +2,11 @@ package service.entities;
 
 public abstract class FSObject {
   private String name;
+  private FSDirectory parent;
 
-  public FSObject(String name) {
+  public FSObject(String name, FSDirectory parent) {
     this.name = name;
+    this.parent = parent;
   }
 
   public String getName() {
@@ -13,5 +15,13 @@ public abstract class FSObject {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public FSDirectory getParent() {
+    return parent;
+  }
+
+  public void setParent(FSDirectory parent) {
+    this.parent = parent;
   }
 }
