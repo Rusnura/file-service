7a0587f1616833f9abc509f860d022609eca3a69
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-06-18 00:17:50 +0700

    WIP: Added structure

--- src/main/java/service/Runner.java
+++ src/main/java/service/Runner.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import service.components.FileSystem;
+import service.entities.FSDirectory;
+import service.entities.FSFile;
 
 @SpringBootApplication
 public class Runner extends SpringBootServletInitializer implements CommandLineRunner {
@@ -18,6 +20,17 @@ public class Runner extends SpringBootServletInitializer implements CommandLineR
 
   @Override
   public void run(String... args) {
-
+    fileSystem.addFSObjectToPath(new FSDirectory("bin"), "/");
+    fileSystem.addFSObjectToPath(new FSDirectory("etc"), "/");
+    fileSystem.addFSObjectToPath(new FSDirectory("home"), "/");
+    fileSystem.addFSObjectToPath(new FSDirectory("rtu"), "/home");
+    fileSystem.addFSObjectToPath(new FSDirectory("mp3"), "/home/rtu");
+    fileSystem.addFSObjectToPath(new FSDirectory("jpg"), "/home/rtu");
+    fileSystem.addFSObjectToPath(new FSFile("KAZKA - Плакала.mp3"), "/home/rtu/mp3");
+    fileSystem.addFSObjectToPath(new FSFile("Captain Panic! - The Observer Redux Original Mix.mp3"), "/home/rtu/mp3");
+    fileSystem.addFSObjectToPath(new FSFile("Legna Zeg - Brave New World.mp3"), "/home/rtu/mp3");
+    fileSystem.addFSObjectToPath(new FSFile("Картинка 1.jpg"), "/home/rtu/jpg");
+    fileSystem.addFSObjectToPath(new FSFile("Картинка 2.jpg"), "/home/rtu/jpg");
+    fileSystem.addFSObjectToPath(new FSFile("Картинка 3.jpg"), "/home/rtu/jpg");
   }
 }
