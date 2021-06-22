e04cd463a2c6327e2b1a11eaf948be0919991a31
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-06-22 21:53:13 +0700

    Added test for non-unique fsObjects added

--- src/main/java/service/Runner.java
+++ src/main/java/service/Runner.java
@@ -20,17 +20,17 @@ public class Runner extends SpringBootServletInitializer implements CommandLineR
 
   @Override
   public void run(String... args) {
-    fileSystem.addFSObjectToPath(new FSDirectory("bin"), "/");
-    fileSystem.addFSObjectToPath(new FSDirectory("etc"), "/");
-    fileSystem.addFSObjectToPath(new FSDirectory("home"), "/");
-    fileSystem.addFSObjectToPath(new FSDirectory("rtu"), "/home");
-    fileSystem.addFSObjectToPath(new FSDirectory("mp3"), "/home/rtu");
-    fileSystem.addFSObjectToPath(new FSDirectory("jpg"), "/home/rtu");
-    fileSystem.addFSObjectToPath(new FSFile("KAZKA - Плакала.mp3"), "/home/rtu/mp3");
-    fileSystem.addFSObjectToPath(new FSFile("Captain Panic! - The Observer Redux Original Mix.mp3"), "/home/rtu/mp3");
-    fileSystem.addFSObjectToPath(new FSFile("Legna Zeg - Brave New World.mp3"), "/home/rtu/mp3");
-    fileSystem.addFSObjectToPath(new FSFile("Картинка 1.jpg"), "/home/rtu/jpg");
-    fileSystem.addFSObjectToPath(new FSFile("Картинка 2.jpg"), "/home/rtu/jpg");
-    fileSystem.addFSObjectToPath(new FSFile("Картинка 3.jpg"), "/home/rtu/jpg");
+//    fileSystem.addFSObjectToPath(new FSDirectory("bin"), "/");
+//    fileSystem.addFSObjectToPath(new FSDirectory("etc"), "/");
+//    fileSystem.addFSObjectToPath(new FSDirectory("home"), "/");
+//    fileSystem.addFSObjectToPath(new FSDirectory("rtu"), "/home");
+//    fileSystem.addFSObjectToPath(new FSDirectory("mp3"), "/home/rtu");
+//    fileSystem.addFSObjectToPath(new FSDirectory("jpg"), "/home/rtu");
+//    fileSystem.addFSObjectToPath(new FSFile("KAZKA - Плакала.mp3"), "/home/rtu/mp3");
+//    fileSystem.addFSObjectToPath(new FSFile("Captain Panic! - The Observer Redux Original Mix.mp3"), "/home/rtu/mp3");
+//    fileSystem.addFSObjectToPath(new FSFile("Legna Zeg - Brave New World.mp3"), "/home/rtu/mp3");
+//    fileSystem.addFSObjectToPath(new FSFile("Картинка 1.jpg"), "/home/rtu/jpg");
+//    fileSystem.addFSObjectToPath(new FSFile("Картинка 2.jpg"), "/home/rtu/jpg");
+//    fileSystem.addFSObjectToPath(new FSFile("Картинка 3.jpg"), "/home/rtu/jpg");
   }
 }
--- src/test/java/service/components/filesystem/FileSystemAddFSObjectToPath.java
+++ src/test/java/service/components/filesystem/FileSystemAddFSObjectToPath.java
@@ -1,13 +1,16 @@
 package service.components.filesystem;
 
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import service.components.FileSystem;
+import service.entities.FSDirectory;
 import service.entities.FSFile;
 import service.entities.FSObject;
 import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 public class FileSystemAddFSObjectToPath {
@@ -24,5 +27,61 @@ public class FileSystemAddFSObjectToPath {
 
     FSObject fileFromFileSystem = fileFromFileSystemOpt.get();
     Assertions.assertTrue(fileFromFileSystem instanceof FSFile);
+  }
+
+  @Test
+  public void addNotUniqueFile() {
+    final String filename = UUID.randomUUID().toString();
+    FSFile file = new FSFile(filename);
+    Assertions.assertTrue(fileSystem.addFSObjectToPath(file, "/"));
+
+    Optional<? extends FSObject> uniqueFileOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(uniqueFileOpt.isPresent());
+
+    FSFile nonUniqueFile = new FSFile(filename);
+    Assertions.assertFalse(fileSystem.addFSObjectToPath(nonUniqueFile, "/"));
+
+    Optional<? extends FSObject> nonUniqueFileOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(nonUniqueFileOpt.isPresent());
+    Assertions.assertEquals(1, FileSystem.ROOT.getChildren().size());
+  }
+
+  @Test
+  public void addNotUniqueDirectory() {
+    final String filename = UUID.randomUUID().toString();
+    FSDirectory directory = new FSDirectory(filename);
+    Assertions.assertTrue(fileSystem.addFSObjectToPath(directory, "/"));
+
+    Optional<? extends FSObject> uniqueFileOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(uniqueFileOpt.isPresent());
+
+    FSDirectory nonUniqueDirectory = new FSDirectory(filename);
+    Assertions.assertFalse(fileSystem.addFSObjectToPath(nonUniqueDirectory, "/"));
+
+    Optional<? extends FSObject> nonUniqueDirectoryOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(nonUniqueDirectoryOpt.isPresent());
+    Assertions.assertEquals(1, FileSystem.ROOT.getChildren().size());
+  }
+
+  @Test
+  public void addNotUniqueFSObject() {
+    final String filename = UUID.randomUUID().toString();
+    FSDirectory directory = new FSDirectory(filename);
+    Assertions.assertTrue(fileSystem.addFSObjectToPath(directory, "/"));
+
+    Optional<? extends FSObject> uniqueDirectoryOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(uniqueDirectoryOpt.isPresent());
+
+    FSFile nonUniqueFile = new FSFile(filename);
+    Assertions.assertFalse(fileSystem.addFSObjectToPath(nonUniqueFile, "/"));
+
+    Optional<? extends FSObject> nonUniqueFileOpt = fileSystem.getFSObjectByPath("/" + filename);
+    Assertions.assertTrue(nonUniqueFileOpt.isPresent());
+    Assertions.assertEquals(1, FileSystem.ROOT.getChildren().size());
+  }
+
+  @AfterEach
+  public void destruct() {
+    FileSystem.ROOT.getChildren().clear();
   }
 }
