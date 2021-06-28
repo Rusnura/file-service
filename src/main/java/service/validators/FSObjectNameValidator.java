4203dc122aaf232107c90bd2d3cf3926c635bf33
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-06-29 00:21:47 +0700

    FileNameValidator fixed

--- src/main/java/service/validators/FSObjectNameValidator.java
+++ src/main/java/service/validators/FSObjectNameValidator.java
@@ -1,12 +1,17 @@
 package service.validators;
 
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import service.entities.FSObject;
 import service.validators.interfaces.IValidator;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FSObjectNameValidator implements IValidator {
+  @SuppressWarnings("ResultOfMethodCallIgnored")
   @Override
   public <T> boolean validate(T object) {
     if (object instanceof FSObject) {
@@ -14,7 +19,12 @@ public class FSObjectNameValidator implements IValidator {
       if (ObjectUtils.isEmpty(fsObject.getName()))
         return false;
 
-
+      try {
+        new File(fsObject.getName()).getCanonicalPath();
+        return true;
+      } catch (IOException e) {
+        // nop
+      }
     }
     return false;
   }
