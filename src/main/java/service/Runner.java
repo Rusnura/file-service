d7739e9d7d89303b8f76869c4a6aadaa5446555c
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-07-04 05:35:57 +0700

    Removed excess code

--- src/main/java/service/Runner.java
+++ src/main/java/service/Runner.java
@@ -10,27 +10,11 @@ import service.entities.FSDirectory;
 import service.entities.FSFile;
 
 @SpringBootApplication
-public class Runner extends SpringBootServletInitializer implements CommandLineRunner {
+public class Runner extends SpringBootServletInitializer {
   @Autowired
   private FileSystem fileSystem;
 
   public static void main(String[] args) {
     SpringApplication.run(Runner.class, args);
-  }
-
-  @Override
-  public void run(String... args) {
-//    fileSystem.addFSObjectToPath(new FSDirectory("bin"), "/");
-//    fileSystem.addFSObjectToPath(new FSDirectory("etc"), "/");
-//    fileSystem.addFSObjectToPath(new FSDirectory("home"), "/");
-//    fileSystem.addFSObjectToPath(new FSDirectory("rtu"), "/home");
-//    fileSystem.addFSObjectToPath(new FSDirectory("mp3"), "/home/rtu");
-//    fileSystem.addFSObjectToPath(new FSDirectory("jpg"), "/home/rtu");
-//    fileSystem.addFSObjectToPath(new FSFile("KAZKA - Плакала.mp3"), "/home/rtu/mp3");
-//    fileSystem.addFSObjectToPath(new FSFile("Captain Panic! - The Observer Redux Original Mix.mp3"), "/home/rtu/mp3");
-//    fileSystem.addFSObjectToPath(new FSFile("Legna Zeg - Brave New World.mp3"), "/home/rtu/mp3");
-//    fileSystem.addFSObjectToPath(new FSFile("Картинка 1.jpg"), "/home/rtu/jpg");
-//    fileSystem.addFSObjectToPath(new FSFile("Картинка 2.jpg"), "/home/rtu/jpg");
-//    fileSystem.addFSObjectToPath(new FSFile("Картинка 3.jpg"), "/home/rtu/jpg");
   }
 }
