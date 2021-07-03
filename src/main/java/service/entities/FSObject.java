da18dff83520e7637b4f2149d6bc7f43f7101157
Ruslan Tumasov <Rus-nura@yandex.ru>
2021-07-04 06:12:43 +0700

    FSObject added 'path' constant, filled by constructor

--- src/main/java/service/entities/FSObject.java
+++ src/main/java/service/entities/FSObject.java
@@ -1,18 +1,27 @@
 package service.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import service.components.FileSystem;
 
 public abstract class FSObject {
   private String name;
+  private final String path;
   private FSDirectory parent;
 
   public FSObject(String name) {
     this.setName(name);
+    if ("".equals(parent.getName())) {
+      this.path = FileSystem.SEPARATOR + name;
+    } else {
+      this.path = parent.getPath() + FileSystem.SEPARATOR + name;
+    }
   }
 
   public String getName() {
     return name;
   }
+
+  public String getPath() { return path; }
 
   public void setName(String name) {
     this.name = name;
